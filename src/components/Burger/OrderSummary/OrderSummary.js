d576c8ac0809a918cde586e36ec093ae77f32c9f
Vaniko <vano.akofa@yahoo.com>
2020-10-08 17:43:30 +0400

    completed burger builder page

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Aux from '../../../hoc/Aux';
+import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) => {
     const ingredientSummary= Object.keys(props.ingredients).map(el => {
@@ -14,7 +15,10 @@ const orderSummary = (props) => {
             <ul>
                 {ingredientSummary}
             </ul>
-            <p>Continue to checkout?</p>
+            <p><strong>Total Price:</strong> {props.price.toFixed(2)} GEL</p>
+            <h3>Continue to checkout?</h3>
+            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
         </Aux>
     );
 };
